d98c23d7741e1def35050b86f04839c42b6d7a62
manikanta0808 <manikantavalipireddy007@gmail.com>
2021-06-07 21:11:12 +0530

    Modified Bubble Sort

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -19,19 +19,4 @@ int main(){
     }printf("\n");
     return 0;
 }
-/*
-    Suppose we have array values 4 3 2 1 5
-    Swaps are done as follows 
-    initial:    4 3 2 5 1
-                3 4 2 5 1
-                3 2 4 5 1
-                3 2 4 1 5 // 5 is placed correctly 
 
-                2 3 4 1 5
-                2 3 1 4 5 // 4 is placed correctly
-
-                2 3 1 4 5
-                2 1 3 4 5 // 3 is placed correctly
-
-                1 2 3 4 5// all the array is sorted                
-*/
